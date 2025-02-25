48070c6e4448c9ce576ff6f152f802094044b62a
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-02-25 20:49:29 +0100

    Add: player crud

--- src/app/api/players/route.js
+++ src/app/api/players/route.js
@@ -17,3 +17,70 @@ export async function GET(request) {
     });
   }
 }
+
+export async function POST(request) {
+  const { firstName, lastName, clubId } = await request.json();
+
+  try {
+    const player = await prisma.player.create({
+      data: { firstName, lastName, clubId },
+    });
+
+    console.log('Player created in database:', player);
+
+    return new Response(JSON.stringify(player), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error creating player:', error);
+    return new Response(JSON.stringify({ error: 'Error creating player' }), {
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(request) {
+  const { id, firstName, lastName, clubId } = await request.json();
+
+  try {
+    const player = await prisma.player.update({
+      where: { id },
+      data: { firstName, lastName, clubId },
+    });
+
+    console.log('Player updated in database:', player);
+
+    return new Response(JSON.stringify(player), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error updating player:', error);
+    return new Response(JSON.stringify({ error: 'Error updating player' }), {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(request) {
+  const { id } = await request.json();
+
+  try {
+    const player = await prisma.player.delete({
+      where: { id },
+    });
+
+    console.log('Player deleted from database:', player);
+
+    return new Response(JSON.stringify(player), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error deleting player:', error);
+    return new Response(JSON.stringify({ error: 'Error deleting player' }), {
+      status: 500,
+    });
+  }
+}
