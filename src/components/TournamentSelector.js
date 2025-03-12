c6fa6347cf69c0cd4e07caa23d8976178a835f9d
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-03-12 08:26:29 +0100

    Update TournamentSelector.js

--- src/components/TournamentSelector.js
+++ src/components/TournamentSelector.js
@@ -1,26 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import "../styles/components/tournamentSelector.css";
-
-export default function TournamentSelector() {
-  const [tournaments, setTournaments] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/tournaments") // Simulatie van database-call
-      .then((res) => res.json())
-      .then((data) => setTournaments(data));
-  }, []);
-
-  return (
-    <div className="tournament-selector">
-      <label className="tournament-label">SELECT TOURNAMENT</label>
-      <select className="tournament-dropdown">
-        {tournaments.map((tournament) => (
-          <option key={tournament.id}>{tournament.name} {tournament.date} {tournament.session}</option>
-        ))}
-      </select>
-    </div>"use client";
-import { useEffect, useState } from "react";
 import useTournamentStore from "../lib/tournamentStore";
 import "../styles/components/tournamentSelector.css";
 
@@ -52,8 +31,5 @@ export default function TournamentSelector() {
         ))}
       </select>
     </div>
-  );
-}
-
   );
 }
