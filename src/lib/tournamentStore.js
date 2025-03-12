9e1e6224e00dba2168f4651c25d35c3824e0c39e
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-03-12 09:00:07 +0100

    Update tournamentStore.js

--- src/lib/tournamentStore.js
+++ src/lib/tournamentStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 const useTournamentStore = create((set) => ({
   selectedTournament: null,
-  setTournament: (tournament) => set({ selectedTournament: tournament }),
+  setSelectedTournament: (tournament) => set({ selectedTournament: tournament }),
 }));
 
 export default useTournamentStore;
