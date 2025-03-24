69369902286487eb72ec7ce567b816a301941eb5
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-03-24 15:35:01 +0100

    Update TeamItem.js

--- src/components/poules/TeamItem.js
+++ src/components/poules/TeamItem.js
@@ -6,12 +6,16 @@ export default function TeamItem({ data, onRemove }) {
   return (
     <ul className="team-list">
       {data.map((team) => (
-        console.log(team),
         <li key={team.id} className="team-item">
           <span>
             {team.player1.firstName} {team.player1.lastName} & {team.player2.firstName} {team.player2.lastName}
           </span>
-          <button onClick={() => onRemove(team.id)} className="delete-btn">X</button>
+          <button 
+            onClick={() => onRemove(team.id)} 
+            className="btn btn-danger"  // Gebruik de btn-stijlen met de btn-danger klasse voor de verwijderknop
+          >
+            X
+          </button>
         </li>
       ))}
     </ul>
