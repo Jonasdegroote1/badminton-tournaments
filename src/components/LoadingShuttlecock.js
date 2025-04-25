557e58ade1152f070dd8cc5e5e5034b4a710de0b
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-04-25 19:21:38 +0200

    update: loader

--- src/components/LoadingShuttlecock.js
+++ src/components/LoadingShuttlecock.js
@@ -1,11 +1,15 @@
 import React from "react";
-import "@/styles/components/loadingShuttlecock.css"; // Zorg ervoor dat het CSS-bestand goed is gekoppeld!
+import "@/styles/components/loadingShuttlecock.css";
 
 export default function LoadingShuttlecock() {
   return (
-    <div className="loading-shuttlecock">
-      <div className="shuttlecock-body">
-        <div className="shuttlecock-feathers"></div>
+    <div className="shuttlecock-loader">
+      <div className="shuttlecock">
+        <div className="feather feather1"></div>
+        <div className="feather feather2"></div>
+        <div className="feather feather3"></div>
+        <div className="feather feather4"></div>
+        <div className="head"></div>
       </div>
     </div>
   );
