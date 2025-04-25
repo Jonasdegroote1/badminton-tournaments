9bfe3d5ad314781a46247b03e3c90b69c070cba9
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-04-25 18:39:48 +0200

    update: prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -3,7 +3,10 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-// Geen globalThis checks nodig bij Edge/serverless
-const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = globalThis
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export { prisma }
