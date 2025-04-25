2b792ed6dc368b1afa2b8660e4c0e09c72ace821
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-04-25 19:14:51 +0200

    update

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,3 @@
-// src/lib/prisma.js
-
 import { PrismaClient } from '@prisma/client/edge'
 
 // Voorkom meerdere Prisma instanties tijdens development (door hot reloads)
@@ -7,11 +5,8 @@ const globalForPrisma = globalThis
 
 const prisma = globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['error'], // je kunt hier ook 'query', 'info' etc. loggen
+    log: [], // Log alle activiteiten
   })
-
-// Log de Prisma Client versie
-console.log('Prisma Client Versie:', prisma._version)  // Dit toont de versie van de Prisma Client
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
