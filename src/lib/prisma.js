6f136147b32f5ebe392f84e6b97a962b97bf653d
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-04-25 19:07:13 +0200

    test

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -10,6 +10,9 @@ const prisma = globalForPrisma.prisma ??
     log: ['error'], // je kunt hier ook 'query', 'info' etc. loggen
   })
 
+// Log de Prisma Client versie
+console.log('Prisma Client Versie:', prisma._version)  // Dit toont de versie van de Prisma Client
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export { prisma }
